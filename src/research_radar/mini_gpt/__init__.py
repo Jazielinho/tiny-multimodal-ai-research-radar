86e26d74892d686c15a761fbed1690927fc5c945
Jazielinho <jahazielponce@gmail.com>
2026-07-01 13:06:01 +0200

    Add Mini GPT package skeleton

---
+++ src/research_radar/mini_gpt/__init__.py
@@ -0,0 +1,10 @@
+"""Tiny Mini GPT experiment package.
+
+This package is intentionally small.
+
+Task 01 should add:
+- tokenizer.py
+- data.py
+
+Do not add model architecture here until tokenizer/data-loader tests pass.
+"""
